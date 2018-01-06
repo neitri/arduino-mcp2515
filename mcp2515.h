6982a0aa6d5b02c13d94eae9579bb7e66d5f0382
Mikhail <neitri@gmail.com>
2018-01-07 00:46:23 +0600

    Update mcp2515.h
    Передача объекта SPIClass в конструктор для совместимости с платами с несколькими SPI портами.

--- mcp2515.h
+++ mcp2515.h
@@ -417,6 +417,7 @@ class MCP2515
         } RXB[N_RXBUFFERS];
 
         uint8_t SPICS;
+        SPIClass *_spi;
 
     private:
 
@@ -434,7 +435,7 @@ class MCP2515
         void prepareId(uint8_t *buffer, const bool ext, const uint32_t id);
     
     public:
-        MCP2515(const uint8_t _CS);
+        MCP2515(const uint8_t _CS, SPIClass *spi);
         ERROR reset(void);
         ERROR setConfigMode();
         ERROR setListenOnlyMode();
